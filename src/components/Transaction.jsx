8ed3eb32ffbcb46bd174d302cb229bea6a2c9b50
Tuấn Hùng Lỗ <lotuanhung.work@gmail.com>
2024-03-02 23:09:25 +0700

    Update Transaction.jsx

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -4,14 +4,13 @@ import { DataGrid } from '@mui/x-data-grid'; // Import DataGrid component for di
 function Transaction() {
     // Define columns for the data grid
     const columns = [
-        { field: 'id', headerName: 'ID', width: 50 },
+        { field: 'id', headerName: 'ID', type: 'number', width: 80 },
         { field: 'purchasedDate', headerName: 'Purchased Date', width: 150 },
         { field: 'name', headerName: 'NFT Name', width: 150 },
-        { field: 'author', headerName: 'Author', width: 200 },
+        { field: 'author', headerName: 'Author', width: 400 },
         { field: 'priceETH', headerName: 'ETH', type: 'number', width: 125 },
         { field: 'priceUSD', headerName: 'USD', type: 'number', width: 150 },
         { field: 'blockchain', headerName: 'Blockchain', width: 150 },
-        { field: 'transactionHash', headerName: 'Transaction Hash', width: 300 },
     ];
 
     // Generate sample data for rows
@@ -20,12 +19,11 @@ function Transaction() {
         const id = i + 1;
         const purchasedDate = new Date(Date.now() - Math.floor(Math.random() * 10000000000)).toISOString().split('T')[0];
         const name = `NFT #${id}`;
-        const author = `0x${Math.floor(Math.random() * 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0).toString(16)}`;
+        const author = `0x${Math.floor(Math.random() * 0xffffffffffffffffffffffffffffffffffffffff0).toString(16)}`;
         const priceETH = Math.floor(Math.random() * 10000) * 0.01;
         const conversionRate = 2265.65;
         const priceUSD = (priceETH * conversionRate).toFixed(2);
         const blockchain = ['Ethereum'][Math.floor(Math.random() * 1)];
-        const transactionHash = `0x${Math.floor(Math.random() * 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0).toString(16)}`;
 
         // Add each row to the rows array
         rows.push({
@@ -36,7 +34,6 @@ function Transaction() {
             priceETH,
             priceUSD,
             blockchain,
-            transactionHash,
         });
     }
 
@@ -49,7 +46,7 @@ function Transaction() {
                     <h1>Transactions History</h1>
                 </Box>
                 {/* DataGrid to display the transaction data */}
-                <Box sx={{ width: 1, height: "fit-content" , maxWidth: "fit-content", mx: "auto"}}>
+                <Box sx={{ width: "100%", height: "fit-content" , maxWidth: "fit-content", mx: "auto"}}>
                     <DataGrid
                         rows={rows}
                         columns={columns}
