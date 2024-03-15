332d13dc1007560f7c7b971d765ad563666b51f9
Hồ Thành An <hothanhan365@gmail.com>
2024-03-15 14:27:18 +0700

    a

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,20 +1,21 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-	const authHeader = req.header('Authorization')
-	const token = authHeader && authHeader.split(' ')[1]
+    const authHeader = req.header('Authorization')
+    const token = authHeader && authHeader.split(' ')[1]
 
-	if (!token) return res.sendStatus(401)
+    if (!token) return res.sendStatus(401)
 
-	try {
-		const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    try {
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
-		req.userId = decoded.id
-		next()
-	} catch (error) {
-		console.log(error)
-		return res.sendStatus(403)
-	}
+        // Use accountId instead of id
+        req.userId = decoded.accountId
+        next()
+    } catch (error) {
+        console.error(error)
+        return res.sendStatus(403)
+    }
 }
 
 module.exports = verifyToken
