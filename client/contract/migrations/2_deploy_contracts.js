4ef8cbd9fd3ecb8047f7020e32fcea8003963c31
Tuấn Hùng Lỗ <lotuanhung.work@gmail.com>
2024-03-19 16:45:35 +0700

    contract

---
+++ client/contract/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Color = artifacts.require("Ownership");
+
+module.exports = function(deployer) {
+    deployer.deploy(Color);
+};
---
+++ client/contract/truffle-config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  
+  networks: {
+    // contract_build_directory: "./contract",
+    AWS:{
+        host: "34.243.239.33",
+        port: 8545,
+        network_id: "9999",
+        from: "0x7cDd2EFdeD0970a9203c54a4bB59d2c1ee570E2F",
+        gas: 5000000 
+    }
+}
+
+  
+}
