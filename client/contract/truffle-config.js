baa8e51f13b1abb275a77ed0cfbcbe3955876942
Tuấn Hùng Lỗ <lotuanhung.work@gmail.com>
2024-03-20 09:11:35 +0700

    Update truffle-config.js

--- client/contract/truffle-config.js
+++ client/contract/truffle-config.js
@@ -7,7 +7,7 @@ module.exports = {
         host: "34.243.239.33",
         port: 8545,
         network_id: "9999",
-        from: "0x7cDd2EFdeD0970a9203c54a4bB59d2c1ee570E2F",
+        from: "0x061E17F6191e596863244cd33076E45663fe0dd1",
         gas: 5000000 
     }
 }
